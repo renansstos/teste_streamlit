aaf92da149396b2f39f9c14f8c5bfe0154b50402
renansstos <84860039+renansstos@users.noreply.github.com>
2023-04-27 10:08:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import numpy as np
 
-st.write('Hello world!')
+st.title("Eigenvalues Calculator")
+
+# Create input fields for the four values
+a = st.number_input("Enter the first value:")
+b = st.number_input("Enter the second value:")
+c = st.number_input("Enter the third value:")
+d = st.number_input("Enter the fourth value:")
+
+# Arrange values in a 2x2 matrix
+A = np.array([[a, b], [c, d]])
+
+# Calculate eigenvalues
+eigenvalues = np.linalg.eigvals(A)
+
+# Print the eigenvalues
+st.write("The eigenvalues of the matrix are:", eigenvalues)
