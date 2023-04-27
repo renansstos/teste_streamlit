c11556e7e58527a48172c8eeb7b893dce045e7c2
renansstos <84860039+renansstos@users.noreply.github.com>
2023-04-27 10:14:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,29 @@ import numpy as np
 
 st.title("Eigenvalues Calculator")
 
-# Create input fields for the four values
-a = st.number_input("Enter the first value:")
-b = st.number_input("Enter the second value:")
-c = st.number_input("Enter the third value:")
-d = st.number_input("Enter the fourth value:")
+# Create a 3x3 matrix input form
+a1, b1, c1, a2, b2, c2, a3, b3, c3 = st.beta_columns(9)
+with a1:
+    a11 = st.number_input(label='', key='a11')
+with b1:
+    a12 = st.number_input(label='', key='a12')
+with c1:
+    a13 = st.number_input(label='', key='a13')
+with a2:
+    a21 = st.number_input(label='', key='a21')
+with b2:
+    a22 = st.number_input(label='', key='a22')
+with c2:
+    a23 = st.number_input(label='', key='a23')
+with a3:
+    a31 = st.number_input(label='', key='a31')
+with b3:
+    a32 = st.number_input(label='', key='a32')
+with c3:
+    a33 = st.number_input(label='', key='a33')
 
-# Arrange values in a 2x2 matrix
-A = np.array([[a, b], [c, d]])
+# Arrange values in a 3x3 matrix
+A = np.array([[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]])
 
 # Calculate eigenvalues
 eigenvalues = np.linalg.eigvals(A)
